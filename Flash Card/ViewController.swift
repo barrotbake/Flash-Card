237ba62746a9fadcc0c8571b9257b775f28cdca8
barrotbake <nguyenjacob411@gmail.com>
2021-02-20 11:23:17 -0800

    First Lab Complete!

--- Flash Card/ViewController.swift
+++ Flash Card/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var frontLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func didTapFlashCard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
 
 }
 
